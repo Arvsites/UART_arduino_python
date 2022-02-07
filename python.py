d67a1e31bb89457f3666eb327495ec9e298840f3
Arvsites <70324628+Arvsites@users.noreply.github.com>
2022-02-07 15:25:20 +0500

    Update python.py

--- python.py
+++ python.py
@@ -4,14 +4,8 @@ import serial
 
 ser = serial.Serial('COM23', 115200) #вместо COM23 необходимо указать номер порта, куда подключена плата
 while True:
-    val = input("Введите число от 1 до 5: ")
+    val = input("Введите число от 1 до 2 ")
     if val == "1":
         ser.write(b'1')
     if val == "2":
         ser.write(b'2')
-    if val == "3":
-        ser.write(b'3')
-    if val == "4":
-        ser.write(b'4')
-    if val == "5":
-        ser.write(b'5')
